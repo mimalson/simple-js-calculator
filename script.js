e145f9a12537beb6821a57320045ac6aef13b232
Amina <87755052+mimalson@users.noreply.github.com>
2022-06-25 11:53:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+const num1 = parseFloat(prompt('Enter the first number:'));
+const num2 = parseFloat(prompt('Enter the second number:'));
+
+const operator = prompt('Enter operator(+,-,/,*');
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input! Refresh the page and provide detail')
+}else{
+    if(operator== '+'){
+        result = num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }else if(operator == '/'){
+        result = num1 / num2;
+    }else if(operator == '*'){
+        result = num1 * num2;
+    }
+    document.write(num1+operator+num2+'=' + result);
+}
